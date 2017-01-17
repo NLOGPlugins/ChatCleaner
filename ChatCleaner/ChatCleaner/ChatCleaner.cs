92f123256af05d863bb44c1852de1e74ed83ddbd
Herb9 <pksxtm@gmail.com>
2017-01-17 14:35:52 +0900

    명령어 추가.

--- ChatCleaner/ChatCleaner/ChatCleaner.cs
+++ ChatCleaner/ChatCleaner/ChatCleaner.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿
+using MiNET;
+
+using MiNET.Plugins;
+
+using MiNET.Plugins.Attributes;
+
+using MiNET.Utils;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +15,23 @@ using System.Threading.Tasks;
 
 namespace ChatCleaner
 {
-    public class Class1
+
+    public class ChatCleaner : Plugin
     {
+
+        [Command]
+        public void clean(Player player)
+        {
+            int i = 0;
+
+            do
+            {
+                player.SendMessage($"{ChatColors.Red}\n");
+
+                ++i;
+            }
+
+            while (i < 30);
+        }
     }
 }
